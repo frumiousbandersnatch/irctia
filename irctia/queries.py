f9ba17cb318a0695526aeb0667a3459b1fd4299c
Frumious Bandersnatch <frumious.irc@gmail.com>
2015-01-18 13:11:59 -0500

    Add formatting option.

--- irctia/queries.py
+++ irctia/queries.py
@@ -16,8 +16,10 @@ import click
               help='Set a pattern to match on the host')
 @click.option('-c','--channel',default="",
               help='Set channel name, default will try to guess from logfile')
+@click.option('-f','--format', 'formatstr', default="{mask}",
+              help='Set the output format using Python str.format()')
 @click.argument('dburl')
-def joins(nick, user, host, channel, dburl):
+def joins(nick, user, host, channel, formatstr, dburl):
     '''
     Find matching joins.
     '''
@@ -29,10 +31,11 @@ def joins(nick, user, host, channel, dburl):
         q = q.filter(Join.user.like('%'+user+'%'))
     if host:
         q = q.filter(Join.host.like('%'+host+'%'))
-    q = q.order_by(Join.stamp)
 
-    masks = set()
-    for row in q:
-        masks.add(row.mask)
-    for m in sorted(masks):
-        click.echo(m)
+    output = set()
+    for obj in q:
+        string = formatstr.format(mask = obj.mask, **obj.__dict__)
+        output.add(string)
+
+    for o in sorted(output):
+        click.echo(o)
